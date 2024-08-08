93f727d64e8f3aa5729b1d58a00d1337cf1f6ee3
Saša Markulin <Sasa.Markulin@asseco-see.hr>
2024-08-08 13:32:51 +0200

    Initial commit

---
+++ bootstrap.php
@@ -0,0 +1,18 @@
+<?php
+
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
+$appEnv = getenv('APP_ENV') ?: 'PROD';
+
+if ($appEnv === 'DEV') {
+    ini_set('display_errors', '1');
+    ini_set('display_startup_errors', '1');
+    error_reporting(E_ALL);
+} else {
+    ini_set('display_errors', '0');
+    ini_set('display_startup_errors', '0');
+    error_reporting(0);
+}
---
+++ public/index.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+require_once __DIR__ . '/../bootstrap.php';
+
+// use App\Core\Router;
+
+// $router = new Router();
+
+require_once __DIR__ . '/../routes/web.php';
+
+// $router->dispatch();
